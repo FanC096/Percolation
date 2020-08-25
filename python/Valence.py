850d2e01d3b1c3dff3a8c60f9b0a97ecbf7a99a9
FanC-06 <qianfan_chen@outlook.com>
2020-08-25 21:43:58 +0800

    Update Valence.py

--- python/Valence.py
+++ python/Valence.py
@@ -1,18 +1,20 @@
 import numpy as np
 import sys
+import matplotlib.pyplot as plt
 
 sys.setrecursionlimit(50000)
 
 count = 0
-k = 4  # valence graph of degree k
+k = 2  # valence graph of degree k
 p = 0.5  # probability
-n = 1000000  # number of trials
-
+n = 10000000  # number of trials
+lst = [None] * n
+dist = [0, 0, 0, 0, 0, 0, 0]
 
 def valence():
     global count
     count += 1
-    for j in range(1,k - 1):
+    for j in range(k - 1):
         r = np.random.uniform(0, 1)
         if r <= p:
             valence()
@@ -22,12 +24,18 @@ def valence():
 s = 0
 for i in range(n):
     count = 1
-    for j in range(1,k):
+    for j in range(k):
         r = np.random.uniform(0, 1)
         if r <= p:
             valence()
     # print(count)
+    lst[i] = count
+    if count <= 7:
+        dist[count - 1] += 1
     s += count
 print(s/n)
+print(dist)
+plt.hist(lst, bins=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
+plt.show()
 
 
